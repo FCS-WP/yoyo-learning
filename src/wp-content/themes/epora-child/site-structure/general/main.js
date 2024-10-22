ee1d040134e0ae677fe092e3327a0f277650e5fd
DevTran <tan.tran@floatingcube.com>
2024-10-22 17:47:21 +0700

    close button prevent default and redirect link to online paper

--- src/wp-content/themes/epora-child/site-structure/general/main.js
+++ src/wp-content/themes/epora-child/site-structure/general/main.js
@@ -4,3 +4,8 @@ $ = jQuery;
 $(document).ready(function () {
   sortDataTable();
 });
+//Close Course Button Prevent defalt link and go to online paper
+document.querySelector('.back-course').addEventListener('click', function(event) {
+  event.preventDefault(); 
+  window.location.href = '/online-paper'; 
+});
