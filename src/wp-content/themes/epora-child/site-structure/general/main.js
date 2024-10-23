141ab3c5ace7a50a05a6474ef1140a539bd63304
DevTran <tan.tran@floatingcube.com>
2024-10-23 08:50:57 +0700

    optimise code close course button

--- src/wp-content/themes/epora-child/site-structure/general/main.js
+++ src/wp-content/themes/epora-child/site-structure/general/main.js
@@ -5,7 +5,13 @@ $(document).ready(function () {
   sortDataTable();
 });
 //Close Course Button Prevent defalt link and go to online paper
-document.querySelector('.back-course').addEventListener('click', function(event) {
-  event.preventDefault(); 
-  window.location.href = '/online-paper'; 
-});
+const backCourseButton = document.querySelector('.back-course');
+if (backCourseButton) {
+  backCourseButton.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    window.location.href = '/online-paper'; 
+  });
+  return;
+}else{
+  return;
+}
